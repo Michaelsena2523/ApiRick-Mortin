bd2b4f07b5023d549f62def542e2b85a2b612dba
Michaelsena2523 <senacelularnet@gmail.com>
2024-11-29 22:28:28 -0400

    mas stylo coloque una imagen

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,20 @@ function DataApi() {
   const [imgPersonaje, setImgPersonaje] = useState("");
   const [textInput, setTextInput] = useState("");
 
+  function alerErro() {
+    Swal.fire({
+      icon: "error",
+      title: "Oops... <br> <br> Este rango de numero no exites <br> <br> El rango de numero llega hasta 826",
+    });
+  }
+
+  function erroInserta() {
+    Swal.fire({
+      icon: "error",
+      title: "Oops... <br> <br> Inserte un numero",
+    });
+  }
+
   function recibirText(textInput) {
     setTextInput(textInput.target.value);
   }
@@ -18,29 +32,37 @@ function DataApi() {
     fetch(`https://rickandmortyapi.com/api/character/${infopNumero}`)
       .then((resultado) => resultado.json())
       .then(function (consulApi) {
-        let nombrePersonaje = consulApi.name;
-        let planetaPersonaje = consulApi.species;
-        let imgPersonaje = consulApi.image;
-        setMostrarName(nombrePersonaje);
-        setMostrarPlaneta(planetaPersonaje);
-        setImgPersonaje(imgPersonaje);
+        if (textInput >= 827) {
+          alerErro()
+        } else if (textInput === "") {
+          erroInserta()
+        } else {
+
+          let nombrePersonaje = consulApi.name;
+          let planetaPersonaje = consulApi.species;
+          let imgPersonaje = consulApi.image;
+          setMostrarName(nombrePersonaje);
+          setMostrarPlaneta(planetaPersonaje);
+          setImgPersonaje(imgPersonaje);
+        }
       });
   }
 
   return (
     <>
-    <div className="contenedor">
-            <h2>Api De Rick & Mortin</h2>
-            <input
-                type="text"
-                value={textInput}
-                onChange={recibirText}
-                placeholder="Ingrese numero"
-            />
-            <button onClick={btnClick}>Buscar Personaje</button>
-            <p>{mostrarName}</p>
-            <p>{mostrarPlaneta}</p>
-            <img src={imgPersonaje} alt="" />
+      <div className="contenedor">
+        <img src="" alt="" className="img2" />
+        <h2>Api De Rick & Mortin</h2>
+        <input
+          type="text"
+          value={textInput}
+          onChange={recibirText}
+          placeholder="Ingrese numero"
+        />
+        <button onClick={btnClick}>Buscar Personaje</button>
+        <p>{mostrarName}</p>
+        <p>{mostrarPlaneta}</p>
+        <img src={imgPersonaje} alt="" className="img1" />
       </div>
     </>
   );
